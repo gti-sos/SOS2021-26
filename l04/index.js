2edbcf5a8bb5b894440099f507b72eca26a79a67
keffren <matsergir1@alum.us.es>
2021-03-13 19:07:56 +0100

    Implementación de l04
    Se ha añadido al repositorio el entorno de JavaScript.

---
+++ l04/index.js
@@ -0,0 +1,21 @@
+//console.log("Hola Mundo");
+
+//Implementación de modulos
+
+//cool-ascii-faces
+var cool = require("cool-ascii-faces");
+console.log("Test del modulo cool: " + cool());
+
+//express -> devuelve una función constructora
+var express = require("express");
+var app = express();
+var port = 1000;
+
+app.get("/cool",(request, response) => {
+    response.send(cool());
+    console.log.apply("New request to /cool has arrived");
+});
+
+app.listen(port, () => {
+    console.log("Server ready listening on port: " + port);
+});
