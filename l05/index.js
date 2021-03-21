2fb70aa3b7b682af51c3bc78d67704f505638428
keffren <matsergir1@alum.us.es>
2021-03-21 13:29:56 +0100

    Update index.js
    Tabla de hostelry añadida

--- l05/index.js
+++ l05/index.js
@@ -144,6 +144,76 @@ border: 1px solid black;
 </html>`);
 });
 
+app.get("/info/hostelry", (req, res) => {
+    res.send(`<!DOCTYPE html>
+    <html>
+<head>
+<style>
+table, th, td {
+    border: 1px solid black;
+}
+</style>
+</head>
+<body>
+
+<h3> <a href=" https://www.ine.es/jaxiT3/Tabla.htm?t=2066&L=0"> Enlace de referencia </a>
+</h3>
+
+<table>
+    <colgroup>
+    <col style="background-color:#f2f2f2">
+    </colgroup>
+    <tr>
+    <th>districts</th>
+    <th>years</th>
+    <th>employee_staff</th>
+    <th>establishments_open </th>
+    <th>travelers_number</th>
+    </tr>
+    <tr>
+    <td>Andalucia</td>
+    <td>2020</td>
+    <td>182,681</td>
+    <td>17,140</td>
+    <td>6,841,779</td>
+    </tr>
+    <tr>
+    <td>Andalucia</td>
+    <td>2019</td>
+    <td>435,130</td>
+    <td>29,557</td>
+    <td>19,869,536</td>
+    </tr>
+
+    <tr>
+    <td>Catalonia</td>
+    <td>2020</td>
+    <td>147,190</td>
+    <td>14,751</td>
+    <td>5,787,656</td>
+    </tr>
+
+    <tr>
+    <td>Catalonia</td>
+    <td>2019</td>
+    <td>418,558</td>
+    <td>27,722</td>
+    <td>20,752,391</td>
+    </tr>
+
+    <tr>
+    <td>Catalonia</td>
+    <td>2018</td>
+    <td>400,203</td>
+    <td>27,213</td>
+    <td>20,045,332</td>
+    </tr>
+</table>
+
+</body>
+</html>`);
+});
+
 //Arrancar server
 app.listen(port, () => {
     console.log("Dentro callback- Server ready on port " +port);
