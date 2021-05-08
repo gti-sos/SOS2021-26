433211eb84afaeb29663580c2133a8c43801cb1c
keffren <matsergir1@alum.us.es>
2021-05-08 15:59:40 +0200

    cambios

--- src/front/hostelriesAPI/HostelriesTable.svelte
+++ src/front/hostelriesAPI/HostelriesTable.svelte
@@ -132,7 +132,7 @@
         }else if(res.status == 404){
             r_hostelries = [];
             console.log("--HostelriesAPI:\n  FrontEnd -> Not found!");
-            outputMsg = "Resultado de la búsqueda: " + Object.keys(r_hostelries).length;
+            outputMsg = "Resultado de la búsqueda: " + Object.keys(r_hostelries).length + " recursos encontrados.";
         }
         else{
             window.alert("ERROR: Compruebe que los valores están correctamente para la búsqueda");
