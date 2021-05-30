8f85f70736567caaa6f2d6501f75758138201f15
keffren <matsergir1@alum.us.es>
2021-05-30 18:53:42 +0200

    Integraciones

---
+++ src/front/hostelriesAPI/Integrations/API07.svelte
@@ -0,0 +1,113 @@
+<script type="text/javascript">
+
+    
+    async function loadGraph() {
+
+        let HostelryData = [];
+        const resData = await fetch("/api/v2/hostelries");
+        HostelryData = await resData.json(); 
+
+        let API07Data = [];
+        const resData07 = await fetch('https://sos2021-07.herokuapp.com/api/v1/integration/rentals');
+        API07Data = await resData07.json();
+
+        let valores = [];
+        let valor = {};
+        let campos = ["Establecimientos abiertos","Turistas(miles)","Renta"];
+
+        API07Data.forEach((x) => {
+            HostelryData.forEach((h) => {
+                if(h.year == 2018){
+                    if(h.district == "Andalucia" && x.autonomous_community== "andalucia"){
+                        valor = {
+                            name:   h.district,
+                            data:   [h.establishment_open,(h.traveler_numer)/1000,x.rent]
+                        }
+                        valores.push(valor);
+                    }
+
+                    if(h.district == "Madrid" && x.autonomous_community== "comunidad_de_madrid"){
+                        valor = {
+                            name:   h.district,
+                            data:   [h.establishment_open,(h.traveler_numer)/1000,x.rent]
+                        }
+                        valores.push(valor);
+                    }
+
+                    if(h.district == "Cataluña" && x.autonomous_community== "cataluna"){
+                        valor = {
+                            name:   h.district,
+                            data:   [h.establishment_open,(h.traveler_numer)/1000,x.rent]
+                        }
+                        valores.push(valor);
+                    }
+                }
+            });
+        });
+        
+        //console.log(valores);
+        
+        Highcharts.chart('container', {
+            chart: {
+                type: 'column'
+            },
+            title: {
+                text: ''
+            },
+            subtitle: {
+                text: 'Comparativa entre la hostelría y la renta de alquileres de algunas comunidades autónomas de España.'
+            },
+            xAxis: {
+                categories: campos,
+                tickmarkPlacement: 'on',
+                title: {
+                    enabled: false
+                }
+            },
+            yAxis: {
+                title: {
+                    text: ''
+                },
+                labels: {
+                    formatter: function () {
+                        return this.value ;
+                    }
+                }
+            },
+            tooltip: {
+                split: true,
+                valueSuffix: ''
+            },
+            plotOptions: {
+                area: {
+                    stacking: 'normal',
+                    lineColor: '#666666',
+                    lineWidth: 1,
+                    marker: {
+                        lineWidth: 1,
+                        lineColor: '#666666'
+                    }
+                }
+            },
+            series: valores
+        });
+        
+    }
+
+</script>
+
+<svelte:head>
+	<script src="https://code.highcharts.com/highcharts.js"></script>
+	<script src="https://code.highcharts.com/modules/exporting.js"></script>
+	<script src="https://code.highcharts.com/modules/export-data.js"></script>
+	<script src="https://code.highcharts.com/modules/accessibility.js" on:load="{loadGraph}"></script>
+</svelte:head>
+
+<main>
+	<h3 style="text-align: center;"> Integración con API 07 SOS2021</h3>
+
+	<figure class="highcharts-figure">
+		<div id="container"></div>
+	</figure>
+
+</main>
---
+++ src/front/hostelriesAPI/Integrations/Home.svelte
@@ -0,0 +1,18 @@
+<script>
+    import Button from "sveltestrap/src/Button.svelte";
+</script>
+
+
+<style>
+    #divButtons{
+        text-align: center;
+    }
+</style>
+
+<main>
+    <div id="divButtons">
+        <Button type="button" color="info" onclick="window.location.href='#/hostelries/integrations/I_API_07'"> Integraciones API 07</Button>
+        <Button type="button" color="info" onclick="window.location.href='#/hostelries/integrations/I_API_30'"> Integraciones API 30</Button>
+    </div>
+
+</main>
