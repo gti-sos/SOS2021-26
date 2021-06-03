b1dfd0f9d4370a78962394e0d70bcd9d2c9f9f02
josemarimaya <josemarimaya@gmail.com>
2021-06-03 20:28:21 +0200

    videito bien refacher

--- src/front/About.svelte
+++ src/front/About.svelte
@@ -8,16 +8,19 @@
         <div class="card border-success mb-3" style="">
                 <div class="card-header bg-transparent border-success" style="text-align: center;">Miembro</div>
                 <div class="card-body text-success">
-                    <img src='https://pbs.twimg.com/media/Edwx26zWsAY1oaK?format=jpg&name=large' style = "max-width: 15rem; margin-bottom: 1%;"alt="">
-                  <link rel='icon' type='image/jpg' href='/josemari.jpg'>
-                  <h5 class="card-title">José María Maya Cano</h5>
+                  <!--<img src='https://pbs.twimg.com/media/Edwx26zWsAY1oaK?format=jpg&name=large' style = "max-width: 15rem; margin-bottom: 1%;"alt="">
+                  <link rel='icon' type='image/jpg' href='/josemari.jpg'>-->
+                  <div class="embed-responsive embed-responsive-16by9 video">
+                    <!-- svelte-ignore a11y-missing-attribute -->
+                    <iframe width="816" height="441" src="https://www.youtube.com/embed/aOgQgWC1TMY" frameborder="0"  allowfullscreen></iframe>
+                </div>
+                  <h5 class="card-title" style="padding-left: 30%; margin-top: 3%;"><a href="https://github.com/josemarimaya">José María Maya Cano </a></h5>
                   <p class="card-text">Desarrollador de la api que hace uso de la base de datos de culturaBASE
-                      en su parte del proyecto podremos ver otras integraciones con otros grupos e interacciones con apis externas 
-                      como la de frases de Chuck Norris
+                      en su parte del proyecto podremos ver otras integraciones con otros grupos usando la misma además del uso de la biblioteca de Amchart con otras Apis Externas
                   </p>
                 </div>
                 <div class="card-footer bg-transparent border-success">
-                    <a href="https://www.youtube.com/watch?v=rkrsYIKMDVY" class="btn btn-info" style="margin-left: 15%;">Enlace de youtube</a>
+                    <a href="https://youtu.be/aOgQgWC1TMY" class="btn btn-info" style="margin-left: 35%;">Enlace de youtube</a>
                 </div>
         </div>
         
@@ -31,7 +34,7 @@
                 <div class="embed-responsive embed-responsive-16by9 video">
                     <!-- svelte-ignore a11y-missing-attribute -->
                     <iframe width="816" height="441" src="https://www.youtube.com/embed/5qap5aO4i9A" frameborder="0"  allowfullscreen></iframe>
-                  </div>
+                </div>
                 
               <h5 class="card-title">Mateo Serna Giraldo</h5>
               <p class="card-text"><i>Desarrollador de la API REST: Hoslteries.<br>Interacción con servicios web en base a la recopilación de información del 
