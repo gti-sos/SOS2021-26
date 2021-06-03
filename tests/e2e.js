1925fc29032af2760e93ac0b96b0b15d24201648
josemarimaya <josemarimaya@gmail.com>
2021-06-03 18:52:10 +0200

    cambios en el e2e

--- tests/e2e.js
+++ tests/e2e.js
@@ -9,11 +9,11 @@ const puppeteer = require('puppeteer');
 
   //Vamos a culturaBASE
   await page.click("body > main > main > ul > li:nth-child(3) > ul > li:nth-child(1) > a");
-  await page.waitForTimeout(1000);
+  //await page.waitForTimeout(1000);
   await page.screenshot({ path: './tests/'+'_CB.png' });
   //Cargamos los datos
   await page.click("body > main > main > main > button");
-  await page.waitForTimeout(1000);
+  //await page.waitForTimeout(1000);
   await page.screenshot({ path: './tests/'+'_CargadoDatos.png' });
   //Enseñamos la grafica con barras de highchart
   await page.click("body > main > main > main > div > button:nth-child(3)");
@@ -24,19 +24,51 @@ const puppeteer = require('puppeteer');
   await page.click("body > main > main > main > div > button:nth-child(4)");
   await page.waitForTimeout(1000);
   await page.screenshot({ path: './tests/'+'_GraficaAmChartCB.png' });
+  
+  //Llegar a about sobre amchart
+  await page.click("body > main > main > button");
+  await page.click("body > main > main > main > div > button.btn.btn-danger");
+  await page.click("body > main > main > ul > li:nth-child(5) > a");
+  await page.screenshot({ path: './tests/'+ 'Nosotros.png' });
+  
+  //Vamos a la gráfica conjunta
+  await page.click("body > main > main > button");
+  await page.click("body > main > main > ul > li:nth-child(6) > a");
+  await page.waitForTimeout(1000);
+  await page.screenshot({ path: './tests/'+ 'Conjunta.png' });
 
-  //Grafica Conjunta de sos2021-26
+  //Vamos al menú de integraciones
+  await page.click("body > main > main > button");
+  await page.click("body > main > main > ul > li:nth-child(7) > a");
+  await page.screenshot({ path: './tests/'+ 'MenuIntegraciones2.png' });
+
+  //Menu de integraciones de CB
+  await page.click("#divButtons > button:nth-child(1)");
+  await page.screenshot({ path: './tests/'+ 'MenuIntegracionesCB.png' });
+
+  /*await page.click("#divButtonsExterna > button");
+  await page.click("body > main > main > button");
+  await page.screenshot({ path: './tests/'+ 'VueltaACasa.png' });*/
+
+  /*//Ir a una integracion externa
+  await page.click("#divButtonsMid > button");
+  //No da lugar a que cargue la grafica pero la comprobacion existe
+  await page.waitForTimeout(5000);
+  await page.screenshot({ path: './tests/'+ 'MenuIntegracionesCBExterna.png' });
+
+  await page.click("body > main > main > button");
+  await page.click("#divButtonsExterna > button");
+  await page.click("body > main > main > button");
+  await page.screenshot({ path: './tests/'+ 'VueltaACasa.png' });*/
+
+  /*//Grafica Conjunta de sos2021-26
   await page.goto('https://sos2021-26.herokuapp.com/#/analytics');
   await page.waitForTimeout(1000);
   await page.screenshot({ path: './tests/'+ 'GraficaConjunta.png' });
 
   await page.goto('https://sos2021-26.herokuapp.com/#/integrations');
   await page.waitForTimeout(1000);
-  await page.screenshot({ path: './tests/'+ 'MenuIntegraciones.png' });
-
-
-  
-
+  await page.screenshot({ path: './tests/'+ 'MenuIntegraciones.png' });*/
   
 
   await browser.close();
